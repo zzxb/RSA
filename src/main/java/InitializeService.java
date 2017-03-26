27ba03474259e4247fd9c33375fc13767776ddf9
zzxb(newmac) <cnhidba@126.com>
2017-03-26 11:48:13 +0800

    初始化工程

---
+++ src/main/java/InitializeService.java
@@ -0,0 +1,14 @@
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InitializeService implements InitializingBean {
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		//服务初始化时生成公钥密钥对
+		RSAUtils.generateKeyPair();	//生成公钥密钥对
+		System.out.println("GenerateKeyPair OK !");
+	}
+
+}
---
+++ target/com/index.jsp
@@ -0,0 +1,6 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+请求实例：http://localhost:8080/service/rsa
+</body>
+</html>
